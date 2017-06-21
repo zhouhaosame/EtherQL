c2aa596790eda8de3f837bdbd37ebfe91a6d8864
Ving <viing937@gmail.com>
2017-06-21 15:02:21 +0800

    add getBlockByRange api

--- src/main/java/edu/suda/ada/rest/BlockController.java
+++ src/main/java/edu/suda/ada/rest/BlockController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 /**
  * Created by LiYang on 2016/11/26.
  */
@@ -24,6 +26,17 @@ public class BlockController {
     public SimpleBlock getBlockByHash(@RequestParam("hash") String hash){
         try {
             return blockAPI.getBlockByHash(hash);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "byrange", method = RequestMethod.POST, produces = "application/json")
+    @ResponseBody
+    public List<SimpleBlock> getBlockByHash(@RequestParam("start") int start, @RequestParam("end") int end){
+        try {
+            return blockAPI.getBlocksByRange(start, end);
         } catch (Exception e) {
             e.printStackTrace();
         }
